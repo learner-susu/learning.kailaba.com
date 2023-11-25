66476fdc5e39e605b3008ec8cf3eca2a6f1a5233
learner-susu <gyawalisusu61@gmail.com>
2023-11-25 22:34:07 +0545

    Changed: deploy.php to redirect to learning site

--- website/deploy.php
+++ website/deploy.php
@@ -3,7 +3,7 @@
 $output = shell_exec('/usr/bin/bash /home3/kailabac/myScripts/EL_Computer/learning-auto-deploy.sh>/tmp/learning-auto-deploy.log');
 // Deploy
 echo "Shell exec executed: $output";
-header("refresh: 7; https://docs.computer.kailaba.com");
+header("refresh: 7; https://learning.kailaba.com");
 
 	echo '<title>New Deployement executing.</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
 	<font size="5" color="#182e7a">Your new deployment is deployed..</font><br /><br />
